c02b1b30acce36dcf269f2c480730ebfbc2afe90
Harsh Jagarwal <harshjagarwal123@gmail.com>
2020-08-01 00:02:11 +0530

    make it better

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ connect.then((db)=>{
 var app = express();
 
 // view engine setup
-
+app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
