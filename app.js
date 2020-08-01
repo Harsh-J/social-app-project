ef35229b11d907b2291a02e4fe59f7c36fd0c4d4
Harsh Jagarwal <harshjagarwal123@gmail.com>
2020-08-01 11:42:01 +0530

    make it better

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(express.static(path.join(__dirname, 'insta/build')));
 if(process.env.NODE_ENV=='production'){
   app.use(express.static('insta/build'))
   const path=require('path')
-  app.get("*",(req,res)=>{
+  app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,'insta','build','index.html'))
   })
 }
