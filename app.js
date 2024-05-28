4668875a90fcd09e577fd4f036059623de847709
Harsh <harshjagarwal123@gmail.com>
2024-05-28 07:12:52 -1200

    change

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors({origin:true}))
 
 app.use(passport.initialize());
 
